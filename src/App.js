27a917df8000538c9251250a93e63ccff57c099a
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-10-05 10:43:39 -0300

    add new feature

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css'
 import ListOfGifs from './components/ListOfGifs'
-import { Route, Link } from 'wouter'
+import GifDetail from './components/GifDetail'
+import NavBar from './components/NavBar'
+import { Route } from 'wouter'
 
 function App () {
   return (
     <div className='App'>
       <section className='App-content'>
-        <Link to='/'><h1 style={{ cursor: 'pointer' }}>App</h1></Link>
-        <Link to='/search/riquelme'>JRR10</Link>
-        <Link to='/search/leo messi'>LM10</Link>
-        <Link to='/search/boca juniors'>Boca</Link>
-        <Link to='/search/argentina'>Argentina</Link>
-        <Link to='/search/futbol'>Futbol</Link>
-        <Route
-          component={ListOfGifs}
-          path='/search/:keyword'
-        />
+        <NavBar />
+        <section className='Gif-container'>
+          <Route
+            component={ListOfGifs}
+            path='/search/:keyword'
+          />
+          <Route
+            component={GifDetail}
+            path='/gif/:id'
+          />
+        </section>
       </section>
     </div>
   )
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import { Link } from 'wouter'
+import Browser from './Browser'
+
+export default function NavBar () {
+  return (
+    <section className='Nav-container'>
+      <Link to='/'><h1 style={{ cursor: 'pointer' }}>App</h1></Link>
+      <Browser />
+      <Link to='/search/riquelme'>JRR10</Link>
+      <Link to='/search/leo messi'>LM10</Link>
+      <Link to='/search/boca juniors'>Boca</Link>
+      <Link to='/search/argentina'>Argentina</Link>
+      <Link to='/search/futbol'>Futbol</Link>
+    </section>
+  )
+}
