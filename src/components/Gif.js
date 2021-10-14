cb00f4a36db4e72d2328fdffa747da099dc32bcf
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-10-14 10:03:01 -0300

    fix: fix style

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,9 +2,11 @@ import { Link } from 'wouter'
 
 export default function Gif ({ id, title, url }) {
   return (
-    <Link to={`/gif/${id}`} className='ListOfGifs-item'>
-      <small>{title}</small>
-      <img alt={title} src={url} />
-    </Link>
+    <div>
+      <Link to={`/gif/${id}`} className='ListOfGifs-item'>
+        <small>{title}</small>
+        <img loading='lazy' alt={title} src={url} />
+      </Link>
+    </div>
   )
 }
